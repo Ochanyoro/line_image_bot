c4da61d18ab61838810b1338ffc012c0e67e5d7d
Ochan <wadaoo0223@icloud.com>
2020-11-26 15:02:53 +0900

    first commit

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+    MessageEvent, TextMessage, TextSendMessage,ImageMessage,ImageSendMessage
 )
 import os
 import pya3rt
@@ -15,8 +15,8 @@ import pya3rt
 app = Flask(__name__)
 
 # 環境変数
-linebot_api = LineBotApi('u5r891X494h4KXpkj7+7qal63kJ57DgarwSvEMKUoJKp2BmFijPpUkYHUvtycQob++FVCrPOlhQ6K0LFBd/PY4nx9CiAUtZ6/IeRdoFtfTjS2mU95/wl/p4bI+j+U6iTGGLBS96kaZHdrbz0a2WlkAdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('861d73d44deaebe2da23ee1a770d0083')
+linebot_api = LineBotApi('00rndh6ND1MSPXKA4yHtOilgY+9zKkDxRk0ICZelnDz2wuEhbVtnTg+U8V9XHIbN8lMNSG1DhGSThv/vIMh66vyAibaE3hfnC2WAZfL+lZphQlcYHM8qCa9pewHzhS1yWhyQWQw+fZCSufYuEWk6kAdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('bb19322823539a5f01a8eca92c13949e')
 
 
 @app.route("/callback", methods=['POST'])
@@ -39,17 +39,19 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    ai_message = talk_ai(event.message.text)
+    #ai_message = talk_ai(event.message.text)
     linebot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=ai_message))
+        ImageSendMessage(original_content_url = 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fdol.ismcdn.jp%2Fmwimgs%2Fa%2Ff%2F-%2Fimg_afa0fad37e6c4d5ce34c01faf54f9e79108563.jpg&imgrefurl=https%3A%2F%2Fdiamond.jp%2Farticles%2F-%2F156184&tbnid=5eUbF4tC8geGtM&vet=12ahUKEwitp_ORwZ_tAhVE6ZQKHYIsAqkQMygAegUIARDWAQ..i&docid=56ZkDmQ8GXH_5M&w=1200&h=630&q=%E7%8A%AC&ved=2ahUKEwitp_ORwZ_tAhVE6ZQKHYIsAqkQMygAegUIARDWAQ',
+                         preview_image_url = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fdol.ismcdn.jp%2Fmwimgs%2Fa%2Ff%2F-%2Fimg_afa0fad37e6c4d5ce34c01faf54f9e79108563.jpg&imgrefurl=https%3A%2F%2Fdiamond.jp%2Farticles%2F-%2F156184&tbnid=5eUbF4tC8geGtM&vet=12ahUKEwitp_ORwZ_tAhVE6ZQKHYIsAqkQMygAegUIARDWAQ..i&docid=56ZkDmQ8GXH_5M&w=1200&h=630&q=%E7%8A%AC&ved=2ahUKEwitp_ORwZ_tAhVE6ZQKHYIsAqkQMygAegUIARDWAQ"))
 
-
+"""
 def talk_ai(word):
     apikey = "DZZcVWVGK24c5c1lm3mup4Mqi3GtlWSC"
     client = pya3rt.TalkClient(apikey)
     reply_message = client.talk(word)
     return reply_message['results'][0]['reply']
+"""
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
